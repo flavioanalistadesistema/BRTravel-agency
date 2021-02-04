c94be7c356889f00f844e0fbce358ee3cc15276b
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-02-04 19:30:26 -0300

    Refactore form_budgets

--- resources/views/site/layouts/_forms/form_budget.blade.php
+++ resources/views/site/layouts/_forms/form_budget.blade.php
@@ -4,54 +4,44 @@
         <div class="col-lg-4">
             <label>Origin</label>
             <div class="form-field">
-                <select name="origin_budgets" id="" class="form-control">
-                    <option value="{{$aeroport->origin_budgets ?? '' }}">
-                        @if(assert($aeroport->origin_budgets))
-                        {{$aeroport->origin_budgets ?? 'Search Destination' }}
-                        @endif
-                    </option>
+                <select name="origin_budgets" id="" class="form-control {{$errors->has('origin_budgets') ? 'border-error-budgets': ''}}">
                     @foreach($aeroports as $aeroport)
                     <option value="{{$aeroport}}" {{ old('origin_budgets', $aeroport ) === $aeroport ? '' : '' }}>{{$aeroport}}</option>
                     @endforeach
                 </select>
                 @if ($errors->has('origin_budgets'))
-                <li>{{ $errors->first('origin_budgets') }}</li>
+                <li class="error_budgets">{{ $errors->first('origin_budgets') }}</li>
                 @endif
             </div>
         </div>
         <div class="col-lg-4">
             <label>Destination</label>
             <div class="form-field">
-                <select name="destination_budgets" id="" class="form-control">
-                    <option selected="selected" value="{{$aeroport->destination_budgets ?? '' }}">
-                        @if(assert($aeroport->destination_budgets))
-                        {{$aeroport->destination_budgets ?? 'Search Destination' }}
-                        @endif
-                    </option>
+                <select name="destination_budgets" id="" class="form-control {{$errors->has('destination_budgets') ? 'border-error-budgets': ''}}">
                     @foreach($aeroports as $aeroport)
                     <option value="{{$aeroport}}" {{ old('destination_budgets', $aeroport ) === $aeroport ? '' : '' }}>{{$aeroport}}</option>
                     @endforeach
                 </select>
                 @if ($errors->has('destination_budgets'))
-                <li>{{ $errors->first('destination_budgets') }}</li>
+                <li class="error_budgets">{{ $errors->first('destination_budgets') }}</li>
                 @endif
             </div>
         </div>
         <div class="col-lg-2">
-            <label for="#">Check-out date</label>
+            <label for="#">Check-in date</label>
             <div class="form-field">
-                <input type="text" name="checkout_in_date_budgets" value="{{ old('checkout_in_date_budgets') }}" autofocus class="form-control checkout_date" placeholder="Check In">
+                <input type="text" name="checkout_in_date_budgets" value="{{ old('checkout_in_date_budgets') }}" autofocus class="form-control checkout_date {{$errors->has('checkout_in_date_budgets') ? 'border-error-budgets': ''}}" placeholder="Check In">
                 @if ($errors->has('checkout_in_date_budgets'))
-                <li>{{$errors->first('checkout_in_date_budgets')}}</li>
+                <li class="error_budgets">{{$errors->first('checkout_in_date_budgets')}}</li>
                 @endif
             </div>
         </div>
         <div class="col-lg-2">
             <label for="#">Check-out date</label>
             <div class="form-field">
-                <input type="text" name="checkout_out_date_budgets" value="{{ old('checkout_out_date_budgets') }}" autofocus class="form-control checkout_date" placeholder="Check Out">
+                <input type="text" name="checkout_out_date_budgets" value="{{ old('checkout_out_date_budgets') }}" autofocus class="form-control checkout_date {{$errors->has('checkout_out_date_budgets') ? 'border-error-budgets': ''}}" placeholder="Check Out {{$errors->has('name_budgets') ? 'border-error-budgets': ''}}">
                 @if ($errors->has('checkout_out_date_budgets'))
-                <li>{{$errors->first('checkout_out_date_budgets')}}</li>
+                <li class="error_budgets">{{$errors->first('checkout_out_date_budgets')}}</li>
                 @endif
             </div>
         </div>
@@ -60,27 +50,27 @@
         <div class="col-lg-2">
             <label for="#"></label>
             <div class="form-fiel">
-                <input type="text" name="name_budgets" value="{{ old('name_budgets') }}" autofocus class="form-control" placeholder="Name">
+                <input type="text" name="name_budgets" value="{{ old('name_budgets') }}" autofocus class="form-control {{$errors->has('name_budgets') ? 'border-error-budgets': ''}}" placeholder="Name">
                 @if ($errors->has('name_budgets'))
-                <li>{{$errors->first('name_budgets')}}</li>
+                <li class="error_budgets">{{$errors->first('name_budgets')}}</li>
                 @endif
             </div>
         </div>
         <div class="col-lg-2">
             <label for="#"></label>
             <div class="form-fiel">
-                <input type="text" name="phone_budgets" value="{{ old('phone_budgets') }}" autofocus class="form-control" placeholder="Phone">
+                <input type="text" name="phone_budgets" value="{{ old('phone_budgets') }}" autofocus class="form-control {{$errors->has('phone_budgets') ? 'border-error-budgets': ''}}" placeholder="Phone">
                 @if ($errors->has('phone_budgets'))
-                <li>{{ $errors->first('phone_budgets') }}</li>
+                <li class="error_budgets">{{ $errors->first('phone_budgets') }}</li>
                 @endif
             </div>
         </div>
         <div class="col-lg-4">
             <label for="#"></label>
             <div class="form-fiel">
-                <input type="text" name="email_budgets" value="{{ old('email_budgets') }}" autofocus class="form-control" placeholder="E-mail">
+                <input type="text" name="email_budgets" value="{{ old('email_budgets') }}" autofocus class="form-control {{$errors->has('email_budgets') ? 'border-error-budgets': ''}}" placeholder="E-mail">
                 @if ($errors->has('email_budgets'))
-                <li>{{$errors->first('email_budgets')}}</li>
+                <li class="error_budgets">{{$errors->first('email_budgets')}}</li>
                 @endif
             </div>
         </div>
